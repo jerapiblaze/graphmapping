a4ca1be2e5d25d92effafac600bff849dac3e810
jerapiblaze <tddt0310@gmail.com>
2024-02-26 00:20:04 +0700

    trainning stuffs

--- TrainDeepQLearn.py
+++ TrainDeepQLearn.py
@@ -6,14 +6,14 @@ import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 def Main():
-    problem = GraphMappingProblem.LoadProblem(".\data\problems\DUMMY\graphmapping_08842ee0.pkl.gz")
+    problem = GraphMappingProblem.LoadProblem(".\data\problems\DUMMY\graphmapping_5425580d.pkl.gz")
     # print(problem.PHY.nodes(data = True))
     # print(problem.SFC_SET.edges(data= True))
     env = DQL.env.StaticMapping2Env(problem.PHY, problem.SFC_SET, {"node_req": "req", "link_req": "req", "node_cap": "cap", "link_cap": "cap"}, 1500, 20)
     # env = gym.make("CartPole-v1")
     obs, info = env.reset()
     # agent = DQL.agent.DeepQlearnAgent(env.obs_space_size, env.action_space_size) eps_decay = 0.9976
-    agent = DQL.agent.DeepQlearnAgent(env.action_space.n, len(obs), gamma=0.8, eps_decay=1000, eps_start=0.9, eps_end=0.05, replay_buffer=10000, batch_size=128, tau=0.005, lr=1e-4)
+    agent = DQL.agent.DeepQlearnAgent(env.action_space.n, len(obs), gamma=0.8, eps_decay=0.9976, eps_start=1, eps_end=0.01, replay_buffer=2000, batch_size=32, tau=0.005, lr=1e-2, hidden_layder_dim=32, update_freq=10)
     trained_agent, reward_values = DQL.agent.TrainAgent(agent, env, 2100, True)
     with open("./debug_dq.csv", "wt") as f:
         f.write("ep,q\n")
--- TrainQLearn.py
+++ TrainQLearn.py
@@ -3,12 +3,12 @@ import Solvers.QLearn as QLearn
 import gymnasium as gym
 
 def Main():
-    problem = GraphMappingProblem.LoadProblem(".\data\problems\DUMMY\graphmapping_08842ee0.pkl.gz")
+    problem = GraphMappingProblem.LoadProblem(".\data\problems\DUMMY\graphmapping_5425580d.pkl.gz")
     # print(problem.PHY.nodes(data = True))
     # print(problem.SFC_SET.edges(data= True))
     env = QLearn.env.StaticMapping2Env(problem.PHY, problem.SFC_SET, {"node_req": "req", "link_req": "req", "node_cap": "cap", "link_cap": "cap"}, 1500, 20)
     
-    agent = QLearn.agent.QLearningAgent(env.obs_space_size, env.action_space_size, alpha=0.009, gamma=0.8, epsilon=0.01)
+    agent = QLearn.agent.QLearningAgent(env.obs_space_size, env.action_space_size, alpha=0.009, gamma=0.8, epsilon_max=1, epsilon_min=0.01, epsilon_decay=0.9954)
     trained_agent, reward_values = QLearn.agent.TrainAgent(agent, env, 1000, True)
     with open("./debug_q.csv", "wt") as f:
         f.write("ep,q\n")
