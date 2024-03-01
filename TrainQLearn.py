95f3ba7cb4f2099b3f8cd757e42060231aa162b4
jerapiblaze <tddt0310@gmail.com>
2024-03-01 18:16:49 +0700

    train qlearn rework

--- TrainQLearn.py
+++ TrainQLearn.py
@@ -1,33 +1,45 @@
 import GraphMappingProblem
 import Solvers.QLearn as QLearn
 import gymnasium as gym
+from utilities.config import ConfigParser
+from utilities.dir import RecurseListDir, CleanDir
 
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
-def Main():
-    problem = GraphMappingProblem.LoadProblem("./data/problems/DUMMY/graphmapping_1114cf92.pkl.gz")
-    # print(problem.PHY.nodes(data = True))
-    # print(problem.SFC_SET.edges(data= True))
-    env = QLearn.env.StaticMapping2Env(problem.PHY, problem.SFC_SET, {"node_req": "req", "link_req": "req", "node_cap": "cap", "link_cap": "cap"}, 1500, 20)
-    
-    n_episodes = 5000
-    alpha = 0.009
-    gamma = 0.8
-    epsilon_max, epsilon_min = 1, 0.01 
-    epsilon_decay = (epsilon_max-epsilon_min)/n_episodes #0.9954
-    
-    agent = QLearn.agent.QLearningAgent(env.obs_space_size, env.action_space_size, 
-                                        alpha=alpha, gamma=gamma, epsilon_max=epsilon_max, 
-                                        epsilon_min=epsilon_min, epsilon_decay=epsilon_decay)
-    trained_agent, reward_values = QLearn.agent.TrainAgent(agent, env, n_episodes, verbose=True, liveview=True)
-    with open("./debug_q.csv", "wt") as f:
-        f.write("ep,q\n")
-        for r in reward_values:
-            f.write(f"{r[0]}, {r[1]}\n")
-    QLearn.agent.SaveAgent("./data/__internals__/QLearn/DUMMY.pkl.gz", trained_agent)
-    pass
+def Main(config):
+    print(config)
+    if config["DELETE_OLD_DATA"]:
+        CleanDir("./data/__internals__/QL")
+    problemset_name = config["PROBLEM_SETNAME"]
+    save_reward = config["SAVE_REWARDS"]
+    liveview, verbose = config["LIVEVIEW"], config["VERBOSE"]
+    problem_dir = os.path.join(f"./data/problems/{problemset_name}")
+    n_episode = config["N_EPISODES"]
+    alpha = config["ALPHA"]
+    gamma = config["GAMMA"]
+    epsilon_start, epsilon_end, epsilon_decay = config["EPSILON_START"], config["EPSILON_END"], config["EPSILON_DECAY"]
+    big_m, beta = config["BIG_M"], config["BETA"]
+    problem_path_list = RecurseListDir(problem_dir, ["*.pkl.gz"])
+    for problem_path in problem_path_list:
+        problem = GraphMappingProblem.LoadProblem(problem_path)
+        print(problem.name)
+        env = QLearn.env.StaticMapping2Env(problem.PHY, problem.SFC_SET, {"node_req": "req", "link_req": "req", "node_cap": "cap", "link_cap": "cap"}, big_m, beta)
+        agent = QLearn.agent.QLearningAgent(env.obs_space_size, env.action_space_size, 
+                                        alpha=alpha, gamma=gamma, epsilon_max=epsilon_start, 
+                                        epsilon_min=epsilon_end, epsilon_decay=epsilon_decay)
+        trained_agent, rewards = QLearn.agent.TrainAgent(agent, env, n_episode, verbose, liveview)
+        model_save_path = os.path.join("./data/__internals__/QLn", f"{problem.name}.pkl.gz")
+        QLearn.agent.SaveAgent(model_save_path, trained_agent)
+        if not save_reward:
+            continue
+        rewards_save_path = os.path.join("./data/__internals__/QL", f"{problem.name}_rewards.csv")
+        with open(rewards_save_path, "wt") as f:
+            f.write("ep, reward\n")
+            for r in rewards:
+                f.write(f"{r[0]}, {r[1]}\n")
 
 if __name__=="__main__":
-    Main()
-    pass
+    config_list = ConfigParser("./configs/QLSettings/dummy.yaml")
+    for config in config_list:
+        Main(config)
