5ec35ca872c1cdebdff3a213a39a2a35474901cf
jerapiblaze <tddt0310@gmail.com>
2024-03-08 10:22:14 +0700

    change to used links per sfc

--- ResultParser.py
+++ ResultParser.py
@@ -9,16 +9,16 @@ from GraphMappingProblem import GraphMappingProblem, LoadProblem
 def CountUsedLinks_Solution(problem:GraphMappingProblem) -> int:
     link_count = 0
     for link in problem.PHY.edges:
-        used_links = [k for k in problem.solution.keys() if str(k).endswith(f"({link[0]},_{link[1]})") and problem.solution[k]]
-        link_count += 1 if len(used_links) else 0
+        used_links = [k for k in problem.solution.keys() if str(k).endswith(f"({link[0]},_{link[1]})") and str(k).startswith("xEdge") and problem.solution[k]]
+        link_count += len(used_links)
     return link_count
 
-def CountUsedNodes_Solution(problem:GraphMappingProblem) -> int:
-    node_count = 0
-    for node in problem.PHY.nodes:
-        used_nodes = [k for k in problem.solution.keys() if str(k).endswith(f"{node}") and problem.solution[k]]
-        node_count += 1 if len(used_nodes) else 0
-    return node_count
+# def CountUsedNodes_Solution(problem:GraphMappingProblem) -> int:
+#     node_count = 0
+#     for node in problem.PHY.nodes:
+#         used_nodes = [k for k in problem.solution.keys() if str(k).endswith(f"{node}") and str(k).startswith("xNode") and problem.solution[k]]
+#         node_count += len(used_nodes)
+#     return node_count
 
 def MpWorker(queue:mp.Queue, result_file:str):
     while queue.qsize():
@@ -32,17 +32,16 @@ def MpWorker(queue:mp.Queue, result_file:str):
         solution_status = solved_problem.solution_status
         obj_value = solved_problem.obj_value
         runtime = solved_problem.solution_time
-        count_links = CountUsedLinks_Solution(solved_problem)
-        count_nodes = CountUsedNodes_Solution(solved_problem)
+        count_links = CountUsedLinks_Solution(solved_problem)/abs(obj_value)
         with open(result_file, "at") as f:
-            f.write(f"{set_name},{solver_name},{problem_name},{status},{solution_status},{int(abs(obj_value))},{round(runtime,3)},{count_nodes},{count_links}\n")
+            f.write(f"{set_name},{solver_name},{problem_name},{status},{solution_status},{int(abs(obj_value))},{round(runtime,3)},{round(count_links,3)}\n")
     pass
 
 def Main():
     solved_problem_paths = RecurseListDir("./data/solutions", ["*.pkl.gz"])
     result_file = os.path.join(f"./data/results/{datetime.datetime.now().strftime('%Y%m%d_%H%M%S')}.csv")
     with open(result_file, "wt") as f:
-        f.write(f"setname,solvername,problemname,status,solutionstatus,objvalue,runtime,usednodescount,usedlinkscount\n")
+        f.write(f"setname,solvername,problemname,status,solutionstatus,objvalue,runtime,usedlinksrate\n")
     q = IterToQueue(solved_problem_paths)
     MultiProcessing(MpWorker, (q, result_file), 4)
 
